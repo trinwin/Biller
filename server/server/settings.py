beedd3039adabf3808a78c78f3a90de59d4d1e23
Trinity Nguyen <thn.trinity@gmail.com>
2020-02-16 15:41:49 -0800

    Set up server, client, db

--- server/server/settings.py
+++ server/server/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bills',
+        'USER':'root',
+        'PASSWORD': 'hellosql',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
