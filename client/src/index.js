8d839ecbb1a820df08ee7747a1952db3b21590b7
Redjay17 <matthew.vu@sjsu.edu>
2020-02-22 17:40:34 -0800

    modified:   package.json 	modified:   src/index.js 	src/header.jsx 	src/login.css 	src/login.jsx

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'antd/dist/antd.css';
+import './login.css';
+import Login from './login';
+import { Form } from "antd";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const LoginForm = Form.create({ name: 'normal_login' })(Login);  
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<LoginForm />, document.getElementById('root'));
