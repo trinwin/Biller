86e83f9fcc7ee9230d71d0ecec5d37e56f220439
Au Tran <xtranx2@yahoo.com>
2020-03-08 14:09:12 -0700

    Test Login FE + BE

--- client/src/api/login.api.js
+++ client/src/api/login.api.js
@@ -2,16 +2,16 @@ import axios from 'axios';
 import { loginSuccessfully, loginFailed } from '../store/actions/auth.action';
 import { HOST, LOGIN_URI, USER_TOKEN } from '../constants';
 import { setTokenToLocalStorage } from '../utils';
-import querystring from 'querystring';
+// import querystring from 'querystring';
 
 // eslint-disable-next-line import/prefer-default-export
 export const login = userData => dispatch => {
   console.log(HOST, LOGIN_URI, userData);
   axios
-    .post(`${HOST}${LOGIN_URI}?` + querystring.stringify(userData))
+    .post(`${HOST}${LOGIN_URI}`, userData)
     .then(res => {
       // Set userToken to Local Storage
-      setTokenToLocalStorage(USER_TOKEN, res.data.token).then(() => {
+      setTokenToLocalStorage(USER_TOKEN, res.data.access_token).then(() => {
         console.log(res);
         dispatch(loginSuccessfully(res.data));
       });
