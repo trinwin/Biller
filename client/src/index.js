0963002f72b70feb9779922680fdf0d97533cebb
Matthew Vu <Matthew.Vu@sjsu.edu>
2020-03-11 15:37:15 -0700

    On branch 10-Register-UI  Your branch is up to date with 'origin/10-Register-UI'.
     Changes to be committed:
    	modified:   src/api/register.api.js
    	modified:   src/index.js
    	modified:   src/pages/HomePage.js
    	modified:   src/pages/LoginPage.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import 'antd/dist/antd.css';
-import { Provider } from 'react-redux';
-import App from './App';
-import Store from './store/store';
-
-=======
 import { Provider } from 'react-redux';
 import App from './App';
 
 // Redux
 import Store from './store/store';
 
->>>>>>> 94a686cca077120d4d64bd1ca07a86072d6f4347
 ReactDOM.render(
   <Provider store={Store}>
     <App />
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -2,43 +2,22 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Redirect, withRouter } from 'react-router-dom';
-<<<<<<< HEAD
-import { Layout } from "antd";
-
-import HeaderPage from "../components/header/Header";
-import { login } from '../api/login.api';
-import Login from '../components/login/Login';
-
-import "./LoginPage.css";
-
-const { Content, Footer } = Layout;
-=======
 
 import { login } from '../api/login.api';
 import Login from '../components/Login.js';
->>>>>>> 94a686cca077120d4d64bd1ca07a86072d6f4347
+import HeaderPage from '../components/header/Header';
 
 class LoginPage extends Component {
   constructor() {
     super();
-<<<<<<< HEAD
-=======
     this.state = {
       email: '',
       password: '',
     };
->>>>>>> 94a686cca077120d4d64bd1ca07a86072d6f4347
 
     this.onClick = this.onClick.bind(this);
   }
 
-<<<<<<< HEAD
-  onClick = e => {
-    console.log("Recieved values: ", e);
-    this.props.login({
-      email: e.username,
-      password: e.password
-=======
   onChange = e => {
     this.setState({ [e.target.id]: e.target.value });
   };
@@ -48,44 +27,24 @@ class LoginPage extends Component {
     this.props.login({
       email: this.state.email,
       password: this.state.password,
->>>>>>> 94a686cca077120d4d64bd1ca07a86072d6f4347
     });
   };
 
   render() {
-<<<<<<< HEAD
-=======
     const email = { ...this.state.email };
     const password = { ...this.state.password };
->>>>>>> 94a686cca077120d4d64bd1ca07a86072d6f4347
     const user = this.props.user || {};
     const { token } = user;
 
     return token ? (
       <Redirect to="/" user={user} />
     ) : (
-<<<<<<< HEAD
-      <Layout>
-        <HeaderPage />
-          <Content style={{ marginTop: "20vh" }}> 
-          <div className = "login-center">
-            <div role="presentation" className = "login-box">
-              <Login 
-                onClick={e => this.onClick(e)}
-              />       
-            </div>
-          </div>
-          </Content>
-        <Footer style={{ textAlign: "center" }}>Copyrights reserved by whatever I think I don't know.</Footer>
-      </Layout>
-=======
       <Login
         onChange={e => this.onChange(e)}
         onClick={e => this.onClick(e)}
         email={email}
         password={password}
       />
->>>>>>> 94a686cca077120d4d64bd1ca07a86072d6f4347
     );
   }
 }
