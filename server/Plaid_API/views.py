e5c9bab72a9240272ca634c6f16e96e2027fdef6
Au Tran <xtranx2@yahoo.com>
2020-04-10 13:56:16 -0700

    Finished all schema, and Plaid API calls

--- server/Plaid_API/views.py
+++ server/Plaid_API/views.py
@@ -5,6 +5,8 @@ from rest_framework.decorators import api_view, authentication_classes, permissi
 from rest_framework import status
 from .models import BankAccounts
 from Authentication.models import User as User_Model
+from .models import Transactions, Bill
+from django.db.models import Sum
 import datetime
 import json
 import time
@@ -16,7 +18,7 @@ PLAID_CLIENT_ID = '5e69b65f584f98001426e625'
 PLAID_SECRET = 'fb3902da2745d2ea1d60dd553fbb47'
 PLAID_PUBLIC_KEY = '716f1a504cda22791ca574fbcb4736'
 
-PLAID_ENV = os.getenv('PLAID_ENV', 'sandbox')
+PLAID_ENV = os.getenv('PLAID_ENV', 'sandboxt')
 PLAID_PRODUCTS = os.getenv('PLAID_PRODUCTS', 'transactions')
 PLAID_COUNTRY_CODES = os.getenv('PLAID_COUNTRY_CODES', 'US')
 PLAID_OAUTH_REDIRECT_URL = os.getenv('PLAID_OAUTH_REDIRECT_URL','')
@@ -57,13 +59,52 @@ def get_access_token(request):
     try:
         # Using the access token, retrieve the user's financial accounts
         response = client.Accounts.get(access_token)
+        start_date = '{:%Y-%m-%d}'.format(datetime.datetime.now() + datetime.timedelta(-180))
+        end_date = '{:%Y-%m-%d}'.format(datetime.datetime.now())
         # For each accounts, create a new BankAccount object in database
         # Save the user's email, access token, and account_id for each object
         for account in response["accounts"]:
-            BankAccounts.objects.create(user = user[0], access_token = access_token, \
-                account_id = account["account_id"])
+
+            # If official_name is not None then use it as bank account name
+            # Otherwise use account['name']
+            if account['official_name'] != None:
+                print("Official Name: ", account['official_name'])
+                new_account = BankAccounts.objects.create(user = user[0], access_token = access_token, \
+                    account_id = account["account_id"], type=account['subtype'], \
+                    name = account["official_name"], balance = account['balances']['current'])
+            else:
+                print("Name: ", account['name'])
+                new_account = BankAccounts.objects.create(user = user[0], access_token = access_token, \
+                    account_id = account["account_id"], type=account['subtype'], \
+                    name = account["name"], balance = account['balances']['current'])
+            
+            # This part for adding a bill object if the account is a credit type
+            if account['subtype'] == 'credit card' or account['subtype'] == 'credit'\
+                    or account['type'] == 'credit card' or account['type'] == 'credit':
+                    Bill.objects.create(account_id = new_account, amount = account['balances']['current'\
+                    ], due_date = None, notified= False)
+
+            # This part is for adding the transactions of each account from the last 180 days
+            transactions = client.Transactions.get(access_token, start_date, end_date, \
+                    account_ids=[account["account_id"]])
+            for transaction in transactions["transactions"]:
+                d = transaction['date'].split("-")
+                date = datetime.date(int(d[0]), int(d[1]), int(d[2]))
+                # If the name of the transaction it > 255
+                # slice it so that the length is 255 max
+                if len(transaction['name']) < 255:
+                    Transactions.objects.create(account_id=new_account, name=transaction['name'],\
+                        category=transaction['category'][0], amount=transaction['amount'],\
+                            date= date, transaction_id = transaction['transaction_id'])
+                else:
+                    Transactions.objects.create(account_id=new_account, name=transaction['name'][0:254],\
+                        category=transaction['category'][0], amount=transaction['amount'],\
+                            date= date, transaction_id = transaction['transaction_id'])           
     except plaid.errors.PlaidError as err:
         return Response({"err": err.message}, status = status.HTTP_403_FORBIDDEN)
+
+    #This is where I get the user's bank accounts and associated transactions to return
+    
 
     return Response({'message': 'Success'})
 
@@ -72,130 +113,153 @@ def get_access_token(request):
 # These 2 decorators are for bypassing JWT tokens for testing purposes
 #@authentication_classes([])
 #@permission_classes([])
+
+# JSON Format is 
+# [[account.name, account.type, account.balance, {transaction.name: [transaction info]}], 
+#  [another list like the one above], ...]
 def get_transactions(request):
 
-    # Provide user's email or error
     email = request.data.get("email")
     if email is None:
         return Response({"err": "Email not provided"}, status= status.HTTP_406_NOT_ACCEPTABLE)
 
-    try:
-        user = User_Model.objects.filter(email = email)
-        # If email is not found in database, error
-        if user is None or len(user) == 0:
-            return Response({"err": "User not found"}, status = status.HTTP_404_NOT_FOUND)
+    user = User_Model.objects.filter(email = email)
+    # If email is not found in database, error
+    if user is None or len(user) == 0:
+        return Response({"err": "User not found"}, status = status.HTTP_404_NOT_FOUND)
 
-        # Get the list of distincts access_token associated with this user
-        access_tokens = BankAccounts.objects.filter(user = user[0]).values("access_token").distinct()
-        # If no access token found, return error
-        if access_tokens is None or len(access_tokens) == 0:
-            return Response({"err": "No account found"}, status=status.HTTP_404_NOT_FOUND)
+    # Query the accounts associated with the user
+    accounts = BankAccounts.objects.filter(user=user[0])
+    response = []
+    # For each account, find the transactions associated with it
+    for account in accounts:
+        transactions = Transactions.objects.filter(account_id = account)
+        if len(transactions) != 0:
+            temp = []
+            # Append the account name, type, balance to the list
+            temp.append(account.name)
+            temp.append(account.type)
+            temp.append(str(account.balance))
+            dict = {}
+            # For each transaction, add it to a dictionary where the key is
+            # the transaction name and the value is a list containing
+            # category, date, and amount
+            for transaction in transactions:
+                dict[transaction.name] = [transaction.category, str(transaction.date), transaction.amount]
+            # Append the dictionary to the list
+            temp.append(dict)
+            # Append the list to the response json
+            response.append(temp)
+    print(response)
 
-        # The start and end date for transactions (how far back we want)
-        start_date = '{:%Y-%m-%d}'.format(datetime.datetime.now() + datetime.timedelta(-30))
-        end_date = '{:%Y-%m-%d}'.format(datetime.datetime.now())
-
-        response = []
-        
-        # For each distinct access token, make an API call to get the transactions
-        for access_token in access_tokens:
-
-            # Make a list of account ids associated with each access token
-            account_ids = BankAccounts.objects.filter(user = user[0], \
-                access_token = access_token["access_token"]).values("account_id")
-            list_of_account_ids = [account_id["account_id"] for account_id in account_ids]
-
-            # Make an API call to get the transactions, and then append the result to 
-            # the response list
-            response.append(client.Transactions.get(access_token["access_token"], start_date,\
-                 end_date, account_ids = list_of_account_ids))
-        
-    except plaid.errors.PlaidError as err:
-        return Response({'message': err.message})
-        
-    """
-    print("Accounts associated with this link")
-    for account in response['accounts']:
-        print("--------------------------------------")
-        print("Type: ", account['subtype'])
-        print("Name: ", account['official_name'])
-        print("Type: ", account['type'])
-        print("Available: ", account['balances']['available'])
-        print("Current balance: ", account['balances']['current'])
-        if account['subtype'] == 'credit':
-            print('Limit: ', account['limit'])
-        print("--------------------------------------")
-    print("Transactions: ")
-    for transaction in response['transactions']:
-        print("-----------------------------------------")
-        print("Categories: ", str([category for category in transaction['category']]))
-        print("Name: ", transaction['name'])
-        print("Payment Channel: ", transaction['payment_channel'])
-        print("Payment Amount: ", transaction['amount'])
-        print("Payment Date: ", transaction['date'])
-        print("Payment Pending: ", transaction['pending'])
-        print("-----------------------------------------")
-    """
-
-    """
-    for transaction in response['transactions']:
-        print(transaction)
-    """
-    return Response({'transactions': response})
+    return Response({'transactions': json.dumps(response)})
 
 @csrf_exempt
 @api_view(['GET'])
 # The 2 decorators below is for bypassing JWT authentication for testing purposes
 #@authentication_classes([])
 #@permission_classes([])
-def get_accounts(request):
+
+def net_worth(request):
+
     # Provide user's email or fail
     email = request.data.get("email")
     if email is None:
         return Response({"err": "Email not provided"}, status= status.HTTP_406_NOT_ACCEPTABLE)
 
-    try:
-        # If email is not found in database, fail
-        user = User_Model.objects.filter(email = email)
-        if user is None or len(user) == 0:
-            return Response({"err": "User not found"}, status = status.HTTP_404_NOT_FOUND)
-        # Get list of distinct access tokens
-        access_tokens = BankAccounts.objects.filter(user = user[0]).values("access_token").distinct()
-        response = []
-        # For each access token, make an API call to get all the accounts associated with it
-        for access_token in access_tokens:
-            response.append(client.Accounts.get(access_token = access_token["access_token"]))
-    except plaid.errors.PlaidError as err:
-        return Response({'message': err.message})
+    # If email is not found in database, fail
+    user = User_Model.objects.filter(email = email)
+    if user is None or len(user) == 0:
+        return Response({"err": "User not found"}, status = status.HTTP_404_NOT_FOUND)
+    # Query the accounts associated with the user
+    accounts = BankAccounts.objects.filter(user = user[0])
 
-    return Response({'accounts': response})
+    net_worth = 0
+    
+    # For each account, add the balance to the total if
+    # the account is not credit card type
+    for account in accounts:
+        if account.type != 'credit card' or account.type != 'credit':
+            net_worth += account.balance
+        else:
+            pass
+
+    return Response({"net worth": net_worth})
+
 
 @csrf_exempt
 @api_view(['GET'])
 # The 2 decorators below are for bypassing JWT authentication for testing purposes
 #@authentication_classes([])
 #@permission_classes([])
-def get_balances(request):
+
+# JSON Format is
+# [{'category': category_name, total: total_amount}, 
+#  {'category2': category2_name, total2: total2_amount}, ...]
+def category_expenses(request):
+ 
     # Provide user's email or fail
     email = request.data.get("email")
     if email is None:
         return Response({"err": "Email not provided"}, status= status.HTTP_406_NOT_ACCEPTABLE)
+    # If email is not found in db, fail
+    user = User_Model.objects.filter(email = email)
 
-    try:
-        # If email is not found in db, fail
-        user = User_Model.objects.filter(email = email)
+    if user is None or len(user) == 0:
+        return Response({"err": "User not found"}, status = status.HTTP_404_NOT_FOUND)
 
-        if user is None or len(user) == 0:
-            return Response({"err": "User not found"}, status = status.HTTP_404_NOT_FOUND)
+    # Query the accounts associated with the user
+    accounts = BankAccounts.objects.filter(user = user[0])
 
-        # Get list of distinct access tokens
-        access_tokens = BankAccounts.objects.filter(user = user[0]).values("access_token").distinct()
-        response = []
-        # For each access token, make an API call to get the balances of all accounts 
-        # associated with this access token
-        for access_token in access_tokens:
-            response.append(client.Accounts.balance.get(access_token = access_token["access_token"]))
-    except plaid.errors.PlaidError as err:
-        return Response({'message': err.message})
-    return Response({'balances': response})
+    # SELECT B.category, Sum('amount')
+    # FROM BankAccount A, Transaction B
+    # WHERE A.account_id = B.account_id AND A.user = email
+    # GROUPBY B.category
+    transactions = Transactions.objects.filter(account_id__in=list(accounts)).\
+                    values('category').order_by('category').annotate(total=Sum('amount'))
 
+    print(list(transactions))
+    print(json.dumps(list(transactions)))
+
+    return Response({'category_expense': json.dumps(list(transactions))})
+
+
+
+@csrf_exempt
+@api_view(['GET'])
+# These 2 decorators are for bypassing JWT tokens for testing purposes
+#@authentication_classes([])
+#@permission_classes([])
+
+# JSON FORMAT IS
+# [{account.name: [bill.amount, bill.due_date, bill.notified]},
+#  {account2.name: [bill2.amount, bill2.due_date, bill2.notified]},...]
+def bills(request):
+
+    email = request.data.get("email")
+    if email is None:
+        return Response({"err": "Email not provided"}, status= status.HTTP_406_NOT_ACCEPTABLE)
+
+    user = User_Model.objects.filter(email = email)
+    # If email is not found in database, error
+    if user is None or len(user) == 0:
+        return Response({"err": "User not found"}, status = status.HTTP_404_NOT_FOUND)
+    # Query for accounts associated with the user
+    accounts = BankAccounts.objects.filter(user=user[0])
+    response = []
+    # For each account, query for associated bills
+    for account in accounts:
+        bill = Bill.objects.filter(account_id = account)
+        
+        # If the length of the queryset is 1, then a bill for that account is found
+        if len(bill) == 1:
+            # If bill due date is None, then return null for due date
+            # else return the actual due date
+            if bill[0].due_date != None:
+                response.append({account.name: [bill[0].amount, str(bill[0].due_date), bill[0].notified]})
+            else:
+                response.append({account.name: [bill[0].amount, bill[0].due_date, bill[0].notified]})
+    print(response)
+    print(json.dumps(response))
+
+    return Response({'bills': json.dumps(response)})
