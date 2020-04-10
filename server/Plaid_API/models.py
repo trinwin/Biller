380eb9a9f91497dcd1538ce0e3c389cc0b4126e7
Au Tran <xtranx2@yahoo.com>
2020-04-10 13:56:16 -0700

    Finished all schema, and Plaid API calls

--- server/Plaid_API/models.py
+++ server/Plaid_API/models.py
@@ -5,4 +5,24 @@ from Authentication.models import User as User_Model
 class BankAccounts(models.Model):
     user = models.ForeignKey(User_Model, on_delete=models.CASCADE)
     access_token = models.CharField(max_length = 100, blank = False)    
-    account_id = models.CharField(max_length = 100, blank= False)
+    account_id = models.CharField(max_length = 100, blank= False, primary_key = True)
+    type = models.CharField(max_length = 100, blank= False)
+    name = models.CharField(max_length = 255, blank= False, null = True)
+    balance = models.FloatField(blank= False)
+
+
+class Transactions(models.Model):
+    category = models.CharField(max_length = 100, blank= False)
+    name = models.CharField(max_length = 255, blank= False, null = True)
+    date = models.DateField(auto_now = False, auto_now_add= False, blank= False)
+    account_id = models.ForeignKey(BankAccounts, on_delete=models.CASCADE)
+    amount = models.FloatField(blank= False)
+    transaction_id = models.CharField(max_length = 100, blank=False, primary_key= True)
+
+class Bill(models.Model):
+    account_id = models.ForeignKey(BankAccounts, on_delete = models.CASCADE)
+    amount = models.FloatField(blank= False)
+    due_date = models.DateField(auto_now = False, auto_now_add = False, null=True, blank=True)
+    notified = models.BooleanField(default = False)
+    def __str__(self):
+        return str(self.due_date) + " " + str(self.amount)
--- server/Plaid_API/urls.py
+++ server/Plaid_API/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^access-token/', views.get_access_token, name="access_token"),
     url(r'^transactions/', views.get_transactions, name="transactions"),
-    url(r'accounts/', views.get_accounts, name="get-accounts"),
-    url(r'balances', views.get_balances, name="balances"),
+    url(r'^categories/', views.category_expenses, name="category-expenses"),
+    url(r'^net-worth/', views.net_worth, name="net-worth"),
+    url(r'^bills/', views.bills, name="bills"),
 ]
