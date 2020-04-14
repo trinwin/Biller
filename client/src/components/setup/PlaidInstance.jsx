c65aef404d7e74a37c9b80ed42a9b0d5b626f365
Trinity Nguyen <thn.trinity@gmail.com>
2020-04-14 00:28:28 -0700

    Fix UI Set up Page

--- client/src/components/setup/PlaidInstance.jsx
+++ client/src/components/setup/PlaidInstance.jsx
@@ -1,36 +1,23 @@
 import React from 'react';
-import { Statistic, Row, Col, Popconfirm, Card  } from 'antd';
-import { MinusCircleOutlined } from '@ant-design/icons';
+import { Statistic, Row, Col, Card } from 'antd';
 
-import "./SetupForm.css";
+import './SetupForm.css';
 
 const PlaidInstance = props => {
-    return (
+  return (
+    <Card>
       <Card>
-        <Card>
-          <Row gutter={16}>
-            <Col span={12}>
-              <Statistic title="Bank" value={props.bankName} />
-            </Col>
-            <Col span={12}>
-              <Statistic title="Number of Accounts" value={props.accountNum} />
-            </Col>
-          </Row>
-        </Card>
-        <div className = "plaid-delete">
-          <Popconfirm
-            title="Are you sure delete this account?"
-            okText="Yes"
-            cancelText="No"
-            onConfirm={props.onDelete}
-          >
-            <MinusCircleOutlined
-              className="dynamic-delete-button"
-            />
-          </Popconfirm>
-        </div>
+        <Row gutter={19}>
+          <Col span={12}>
+            <Statistic title="Company" value={props.bankName} />
+          </Col>
+          <Col span={12}>
+            <Statistic title="Number of Accounts" value={props.accountNum} />
+          </Col>
+        </Row>
+      </Card>
     </Card>
-    );
-}
+  );
+};
 
-export default PlaidInstance
+export default PlaidInstance;
--- client/src/components/setup/SetupForm.jsx
+++ client/src/components/setup/SetupForm.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { Button, List } from 'antd';
-import { PlusOutlined } from '@ant-design/icons';
+import { List, Button } from 'antd';
+import { PlusOutlined, ArrowRightOutlined } from '@ant-design/icons';
+import { PlaidLink } from 'react-plaid-link';
+
 import PlaidInstance from './PlaidInstance';
 import './SetupForm.css';
 
@@ -8,65 +10,90 @@ class SetupForm extends Component {
   constructor() {
     super();
     this.state = {
-      forms: [{
-        accountNum: "123",
-        bankName: "Test Bank",
-      }]
-    }
+      forms: [],
+      has_profile: false,
+    };
 
     this.onSuccess = this.onSuccess.bind(this);
     this.onDelete = this.onDelete.bind(this);
   }
 
   componentDidMount() {
-    //Load from database all user accounts
+    // Load from database all user accounts
+    document.body.style.backgroundColor = '#F0F2F5';
   }
 
-  onAdd = () => {
-    this.setState(previousState => ({
-      forms: [...previousState.forms, {
-        accountNum: metadata.accounts.length,
-        bankName: metadata.institution.name,
-      }]
-    }));
+  onDelete = () => {
+    // Delete Function
+    console.log('Deleted!');
   };
+
+  onSuccess(token, metadata) {
+    console.log(token);
+    this.state.has_profile = true;
+
+    this.setState(previousState => ({
+      forms: [
+        ...previousState.forms,
+        {
+          accountNum: metadata.accounts.length,
+          bankName: metadata.institution.name,
+        },
+      ],
+    }));
+  }
 
   render() {
     return (
-
-    <div style={{margin:"5vh"}} >
-      <List
-        grid={{
-          gutter: [48, 16],
-          xs: 1,
-          sm: 1,
-          md: 1,
-          lg: 2,
-          xl: 2,
-          xxl: 2,
-        }}
-        dataSource={this.state.forms}
-        renderItem={item => (
-          <List.Item>
-            <PlaidInstance
-              bankName = {item.bankName}
-              accountNum = {item.accountNum}
-              onDelete = {this.onDelete}
-            />
-          </List.Item>
-        )}
-      />
-      <div className="plaid-div" >
-        <PlaidLink
-          style={{ padding: '20px', fontSize: '16px', cursor: 'pointer', border: 'dashed 2px rgb(120, 120, 120)' }}
-          clientName = 'SJSU-Biller-Project'
-          env = 'sandbox'
-          product = {['auth', 'transactions']}
-          publicKey = '716f1a504cda22791ca574fbcb4736'
-          onSuccess = {this.onSuccess}
-        >
-          <PlusOutlined /> Connect with Plaid
-        </PlaidLink>
+      <div style={{ margin: '5vh' }}>
+        <List
+          grid={{
+            gutter: [48, 16],
+            xs: 1,
+            sm: 1,
+            md: 1,
+            lg: 2,
+            xl: 2,
+            xxl: 2,
+          }}
+          dataSource={this.state.forms}
+          renderItem={item => (
+            <List.Item>
+              <PlaidInstance
+                bankName={item.bankName}
+                accountNum={item.accountNum}
+                onDelete={this.onDelete}
+              />
+            </List.Item>
+          )}
+        />
+        <div className="plaid-div">
+          <PlaidLink
+            style={{
+              padding: '14px',
+              fontSize: '16px',
+              cursor: 'pointer',
+              border: 'dashed 2px rgb(120, 120, 120)',
+            }}
+            clientName="SJSU-Biller"
+            env="sandbox"
+            product={['auth', 'transactions']}
+            publicKey="716f1a504cda22791ca574fbcb4736"
+            onSuccess={this.onSuccess}
+          >
+            <PlusOutlined /> Connect with Plaid
+          </PlaidLink>
+          {this.state.has_profile && (
+            <Button
+              type="primary"
+              icon={<ArrowRightOutlined />}
+              size="large"
+              className="connect-dashboard-btn"
+            >
+              Proceed to Dashboard
+            </Button>
+          )}
+        </div>
       </div>
     );
   }
